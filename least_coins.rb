f088d66ea834826c924e27f97fad8c319896232b
Siri-Dandu <sdandu99@gmail.com>
2018-07-27 17:46:39 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -2,6 +2,17 @@
 
 def least_coins(cents)
 
-#Code your answer here!
-
+if c
+     if number % 5 == 0 && number % 3 == 0
+    puts"FizzBuzz"
+    elsif number % 3 == 0 
+    puts"Fizz"
+    elsif number % 5 == 0 
+    puts"Buzz"
+    else
+    puts number
+  end
+end 
+end 
+counter
 end
